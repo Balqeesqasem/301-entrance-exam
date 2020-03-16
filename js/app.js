bd5b89bd5deeb2023a1842291a3494975e7a476b
Balqeesqasem <balqeesmohammadq.95@gmail.com>
2020-03-16 15:36:34 +0200

    tring in event

--- js/app.js
+++ js/app.js
@@ -5,24 +5,41 @@ function ToDo (toDo , date){
     this.date = date;
 }
 // console.log(toDo);
-function obj() {
-    var toDo = document.getElementById('toDo');
+
+var doSubmit = document.getElementById('form');
+doSubmit.addEventListener('submit', form); 
+
+function form(e)
+{
+    // console.log(event)
+    // var toDoTag = document.getElementById('form');
+    var toDo= document.getElementById('toDo').e.target.toDo;
     console.log(toDo);
-    var date =document.getElementById('date').value;
-    console.log(date)
+    var date = document.getElementById('date').e.target.date;
     var plane = new ToDo (toDo , date);
     toDoArray.push(plane);
-    
+    console.log(toDo);
+    console.log(toDoArray);
 }
-obj();
+form();
+
+// function obj() {
+//     var toDo = document.getElementById('toDo');
+//     console.log(toDo);
+//     var date =document.getElementById('date').value;
+//     console.log(date)
+//     var plane = new ToDo (toDo , date);
+//     toDoArray.push(plane);
+    
+// }
+// obj();
 // function toDoThing (e){
 // var sub = document.getElementById('form')
 // ToDo.addToDo(e.target.ToDo);
 // console.log(e.target.ToDo);
 // }
 
-var doSubmit = document.getElementById('form');
-doSubmit.addEventListener('submit', form);
+
 
 function renderResult()
 {
